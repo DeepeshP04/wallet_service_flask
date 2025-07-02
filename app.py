c00e506fbcebb43a22287c3b3a58e9eca9a1b710
DeepeshP04 <pratapdeepesh4@gmail.com>
2025-07-02 16:03:33 +0530

    Inegrate flask migrate in app for db tables

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 from flask import Flask
 from extensions import db
+from flask_migrate import Migrate
 
-# Create app and configure database
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///wallet.db'
-db.init_app(app)
+def create_app():
+    # Create app and configure database
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///wallet.db'
+    db.init_app(app)
+    migrate = Migrate(app, db)
 
-# Create tables
-with app.app_context():
-    print("Creating tables...")
-    db.create_all()
+    # Create tables
+    with app.app_context():
+        db.create_all()
+        print("Tables created successfully")
+
+    return app
+
+app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
