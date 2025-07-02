bcf561df829525a0d2df799c3a73a3fa3a49a6dc
DeepeshP04 <pratapdeepesh4@gmail.com>
2025-07-02 15:48:19 +0530

    Rename database.py to extensions.py, create db tables

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask
+from extensions import db
 
+# Create app and configure database
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///wallet.db'
+db.init_app(app)
+
+# Create tables
+with app.app_context():
+    print("Creating tables...")
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
